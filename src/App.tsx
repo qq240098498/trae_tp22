d4ef5e53375f10b35e1cd84072596275b4422479
qq240098498 <240098498@qq.com>
2026-06-14 11:54:02 +0800

    .1523281629553499:70ce21121ee4ad4f1904285825fe7e28_6a2e130fa23ce68ff1e0f2be.6a2e2468a23ce68ff1e0fcde.6a2e2468e01773bbaed9e63e:Trae CN.T(2026/6/14 11:47:52)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import ToolListPage from "@/pages/ToolListPage";
 import ToolFormPage from "@/pages/ToolFormPage";
 import ToolDetailPage from "@/pages/ToolDetailPage";
+import { useToolStore } from "@/store/toolStore";
 
 export default function App() {
+  const hydrate = useToolStore((s) => s.hydrate);
+  const updateBorrowStatuses = useToolStore((s) => s.updateBorrowStatuses);
+
+  useEffect(() => {
+    hydrate();
+    updateBorrowStatuses();
+
+    const timer = setInterval(() => {
+      updateBorrowStatuses();
+    }, 60000);
+
+    return () => clearInterval(timer);
+  }, [hydrate, updateBorrowStatuses]);
+
   return (
     <Router>
       <Routes>
